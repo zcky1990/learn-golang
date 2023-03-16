b9eff2da0c4debf0027e8f20d267ffca6aec1dd6
soeltan zaki rizaldy <zcky1990@gmail.com>
2023-03-16 20:17:20 +0700

    add subpackage import example

--- mainapp.go
+++ mainapp.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	
 	"lumachrome.com/main/mypackage"
+	"lumachrome.com/main/mypackage/mysubpackage"
 )
 
 func main() {
     fmt.Println("Hello, world.")
 	mypackage.Package()
+	mysubpackage.Mysubpackage()
 }
---
+++ mypackage/mysubpackage/mysubpackage.go
@@ -0,0 +1,7 @@
+package mysubpackage
+
+import "fmt"
+
+func Mysubpackage() {
+    fmt.Println("Hello, world. from sub-package")
+}
