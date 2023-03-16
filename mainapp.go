8725eb2d789f033a792ff4a204fee248cd3a90d3
soeltan zaki rizaldy <zcky1990@gmail.com>
2023-03-16 20:16:54 +0700

    add package example

--- mainapp.go
+++ mainapp.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	
+	"lumachrome.com/main/mypackage"
+)
 
 func main() {
     fmt.Println("Hello, world.")
+	mypackage.Package()
 }
---
+++ mypackage/package.go
@@ -0,0 +1,7 @@
+package mypackage
+
+import "fmt"
+
+func Package() {
+    fmt.Println("Hello, world. from package")
+}
